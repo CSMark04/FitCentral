84fddfece1b2e4efbf4c53383dca7029399549c7
CSMark04 <Markobonan@gmail.com>
2023-03-19 20:45:43 -0600

    Changed hero section, added nav'

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './App.css'
 import { Box } from '@mui/material'
 import ExerciseDetail from './pages/ExerciseDetail'
 import Home from './pages/Home'
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 
 const App = () => {
   return (
     <Box width="400px" sx={{ width: { xl: '1488px' }}} m='auto'>
-        <Navbar />
+
         <Routes>
           // path = URL, and the element is what is being rendered when taken to that URL
             <Route path="/" element ={<Home />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,230 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Stack } from '@mui/material'
-import Logo from '../assets/icons/blackLogo.svg'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import { Stack } from '@mui/material'
+import Logo from "../assets/icons/logo1.png";
 
-const Navbar = () => {
+// const Navbar = () => {
+//   return (
+//     <Stack
+//     direction='row'
+//     justifyContent='space-between'
+//     sx={{gap:{sm:'122px', xs:'40px'}, mt:{sm:'32px', xs:'20px'}, justifyContent:'none'}} px='20px'
+//     backgroundColor='black'>
+
+//       <Link to='/'>
+//         <img src={Logo} alt='logo' style= {{width: '30%', height: '30%', margin:'0 20px'}} //Link to logo
+//         />
+//       </Link>
+//       <Stack
+//       direction="row"
+//       gap="40px"
+//       fontSize='24px'
+//       alignItems='flex-end'>
+//         <Link to='/' style={{textDecoration: 'none', color:'#3A1212', borderBottom:'3px solid #FF2625'}}>Home</Link>
+//         <a href='#exercises' style={{textDecoration: 'none', color:'#3A1212'}}>Exercises</a>
+
+//       </Stack>
+//     </Stack>
+//   )
+// }
+
+// export default Navbar
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const pages = ["Exercises", "Tools", "Workout", "Diet Planner"];
+const settings = ["Coming Soon"];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Stack // Stack containing all nav elements. Also has css to adjust based on screen sizes
-    direction='row'
-    justifyContent='space-between'
-    sx={{gap:{sm:'122px', xs:'40px'}, mt:{sm:'32px', xs:'20px'}, justifyContent:'none'}} px='20px'>
-      <Link to='/'> 
-        <img src={Logo} alt='logo' style= {{width: '70%', height: '70%', margin:'0 20px'}} //Link to logo
-        /> 
-      </Link>
-      <Stack
-      direction="row"
-      gap="40px"
-      fontSize='24px'
-      alignItems='flex-end'>
-        <Link to='/' style={{textDecoration: 'none', color:'#3A1212', borderBottom:'3px solid #FF2625'}}>Home</Link>
-        <a href='#exercises' style={{textDecoration: 'none', color:'#3A1212'}}>Exercises</a>
+    <AppBar
+      position="static"
+      style={{ background: "transparent", boxShadow: "none" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <img src={Logo} style={{ width: "30%" }} />
+          </Typography>
 
-      </Stack>
-    </Stack>
-  )
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+              page === 'Exercises' ? (<Button
+                key={page}
+                href={`#${page.toLowerCase()}`}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "black",
+                  display: "block",
+                  fontSize: "1rem",
+                }}
+              >
+                {page}
+              </Button>) : 
+               
+              <Button
+                key={page}
+                href={`#${page.toLowerCase()}`}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "black",
+                  display: "block",
+                  fontSize: "1rem",
+                }}
+              >
+                {page}(Coming Soon)
+              </Button>
+
+            ))}
+            </Menu>
+            <Box alignItems="center" justifyContent="center" display="flex">
+              <img src={Logo} style={{ width: "50%" }} />
+            </Box>
+          </Box>
+
+          <Box
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+            justifyContent="space-around"
+          >
+            {pages.map((page) => (
+              page === 'Exercises' ? (<Button
+                key={page}
+                href={`#${page.toLowerCase()}`}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  fontSize: "1rem",
+                }}
+              >
+                {page}
+              </Button>) : 
+               <Tooltip title="Coming Soon" key={page}>
+              <Button
+                key={page}
+                href={`#${page.toLowerCase()}`}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  fontSize: "1rem",
+                }}
+              >
+                {page}
+              </Button>
+              </Tooltip>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-export default Navbar
+export default Navbar;
